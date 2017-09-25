31b90f695720450541d2727ec62ef43ddffc1483
Rodrigo Dugin <rodrigodugin@MacBook-Pro-de-Rodrigo-2.local>
2017-09-25 16:45:51 -0300

    added add or subtract book, improved layout

--- src/__tests__/Book.spec.js
+++ src/__tests__/Book.spec.js
@@ -22,10 +22,50 @@ describe('Book', () => {
 
     });
 
+    it('renders discount price and tag if exists', () => {
+        expect(book.find('.Book__valueContainer__discount').exists()).toBe(true);
+        expect(book.find('.Book__figureContainer__discountTag').exists()).toBe(true);
+    });
+
     it('renders quantity and `Adicionar ao Carrinho` button ', () => {
         expect(book.find('.Book__actions__quantityContainer__minus').exists()).toBe(true);
         expect(book.find('.Book__actions__quantityContainer__quantity').exists()).toBe(true);
         expect(book.find('.Book__actions__quantityContainer__plus').exists()).toBe(true);
         expect(book.find('.Book__actions__addToCart').text()).toBe('Adicionar ao Carrinho');
-    })
-})
+    });
+
+    describe('Add/Subtract buttons', () => {
+
+        const plusBtn = book.find('.Book__actions__quantityContainer__plus');
+        const minusBtn = book.find('.Book__actions__quantityContainer__minus');
+
+        beforeEach(() => {
+            book.setState({quantity: 1});
+        });
+
+        it('adds one on plus button click ', () => {
+            expect(plusBtn.length).toEqual(1);
+            plusBtn.simulate('click');
+
+            expect(book.state().quantity).toEqual(2);
+        });
+
+        it('do not subtract on minus button if quantity == 1', () => {
+            expect(plusBtn.length).toEqual(1);
+            expect(minusBtn.length).toEqual(1);
+
+            minusBtn.simulate('click');
+
+            expect(book.state().quantity).toEqual(1);
+        });
+
+        it('subtract one on minus button click if quantity > 1', () => {
+            book.setState({quantity: 3});
+
+            expect(minusBtn.length).toEqual(1);
+            minusBtn.simulate('click');
+
+            expect(book.state().quantity).toEqual(2);
+        });
+    });
+});
--- src/containers/book/Book.js
+++ src/containers/book/Book.js
@@ -4,11 +4,28 @@ import './Book.css';
 import NumberFormat from 'react-number-format'
 
 export class Book extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {quantity: 1};
+    }
+
+    setPrice = () => {
+        return this.props.book.discount ? this.props.book.price - this.props.book.discount : this.props.book.price;
+    };
+
+    setDiscountTag = () => {
+        return `${Math.ceil((this.props.book.discount / this.props.book.price) * 100)}% OFF`
+    };
+
 
     render() {
         return (
             <div className="Book container">
                 <figure className="Book__figureContainer">
+                    {this.props.book.discount && (
+                        <div className="Book__figureContainer__discountTag">{this.setDiscountTag()}</div>
+                    )}
                     <img src={this.props.book.imageLinks.thumbnail} className="Book__figureContainer__img" alt=""/>
                     <figcaption className="Book__figureContainer__caption">
                         <h5 className="Book__figureContainer__caption__name"> {this.props.book.title}</h5>
@@ -17,18 +34,31 @@ export class Book extends Component {
                 </figure>
 
                 <section className="Book__valueContainer">
+                    {this.props.book.discount && (
+                        <NumberFormat className="Book__valueContainer__discount" value={this.props.book.price}
+                                      decimalPrecision={2} displayType={'text'} decimalSeparator=',' prefix={'R$'}/>
+                    )}
 
-                    <NumberFormat className="Book__valueContainer__price" value={this.props.book.price}
+                    <NumberFormat className="Book__valueContainer__price" value={this.setPrice()}
                                   decimalPrecision={2} displayType={'text'} decimalSeparator=',' prefix={'R$'}/>
-
-
                 </section>
 
                 <section className="Book__actions">
                     <div className="Book__actions__quantityContainer">
-                        <button className="Book__actions__quantityContainer__minus"> - </button>
-                        <span className="Book__actions__quantityContainer__quantity"> 1 </span>
-                        <button className="Book__actions__quantityContainer__plus "> + </button>
+                        <button onClick={() => this.setState(state => {
+                                state.quantity > 1 ? state.quantity-- : 1
+                            }
+                        )} className="Book__actions__quantityContainer__minus"> -
+                        </button>
+
+                        <span className="Book__actions__quantityContainer__quantity"> {this.state.quantity} </span>
+
+                        <button onClick={() => this.setState(state => {
+                                state.quantity++
+                            }
+                        )} className="Book__actions__quantityContainer__plus "> +
+                        </button>
+
                     </div>
                     <button className="Book__actions__addToCart">
                         Adicionar ao Carrinho
